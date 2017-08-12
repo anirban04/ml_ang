252e007cee6bda2d40af3d3e58c3ecf5a58ce25e
Anirban Ray <anirban04@gmail.com>
2017-08-12 10:31:12 -0700

    Assignment 5: Solved

--- assignment5/machine-learning-ex5/ex5/linearRegCostFunction.m
+++ assignment5/machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -11,24 +11,28 @@ m = length(y); % number of training examples
 % You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost and gradient of regularized linear 
 %               regression for a particular choice of theta.
 %
 %               You should set J to the cost and grad to the gradient.
 %
+hTheta = X * theta;
+sqErr = (1 / (2 * m)) * ((hTheta - y) .^ 2);
+sumSqErr = sum(sqErr);
+thetaSq = theta .^ 2;
+%Do not regularize theta(0)
+sumThetaSq = sum(thetaSq) - thetaSq(1);
+regParam = (lambda / (2 * m)) * (sumThetaSq);
+J = sumSqErr + regParam;
 
+err1 = (hTheta - y) .* X(1);
+grad(1) = (1 / m) * sum(err1);
 
-
-
-
-
-
-
-
-
-
+for j=2:columns(X)
+errOthers = (hTheta - y) .* X(:, j);
+grad(j) = ((1 / m) * sum(errOthers)) + ((lambda / m) * theta(j));
+end
 
 % =========================================================================
 
--- assignment5/machine-learning-ex5/ex5/polyFeatures.m
+++ assignment5/machine-learning-ex5/ex5/polyFeatures.m
@@ -7,19 +7,17 @@ function [X_poly] = polyFeatures(X, p)
 
 
 % You need to return the following variables correctly.
-X_poly = zeros(numel(X), p);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Given a vector X, return a matrix X_poly where the p-th 
 %               column of X contains the values of X to the p-th power.
 %
-% 
-
-
-
-
-
-
+%
+X_poly = X;
+for i=2:p
+	tempVect = X .^i;
+	X_poly = [X_poly, tempVect];
+end
 % =========================================================================
 
 end
